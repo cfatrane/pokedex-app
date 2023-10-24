732531e165dc10a4122613cac844f4564cc9e2d6
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2023-10-24 23:09:42 +1100

    setup eslint, absolute import, add axios and some lib for navigation

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            api: './api',
+            assets: './assets',
+            components: './components',
+            constants: './constants',
+            screens: './screens',
+            type: './type',
+            utils: './utils',
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+        },
+      ],
+    ],
   };
 };
