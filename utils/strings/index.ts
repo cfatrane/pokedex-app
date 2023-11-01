796175624fb28a6d9397e9fe2029a8aaa63ab688
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2023-11-01 17:42:12 +1100

    update the capitalize method

--- utils/strings/index.ts
+++ utils/strings/index.ts
@@ -2,8 +2,8 @@ export const stringsToJson = (data: object) => {
   return JSON.stringify(data, null, 2);
 };
 
-export const toCapitalize = ([first = '', ...rest]) => {
-  return first.toUpperCase() + rest.join('').toLowerCase();
+export const toCapitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
 export const consoleLogStringsToJson = (data: object) => {
